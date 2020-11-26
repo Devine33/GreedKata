80f110fd04c95c4f230e90dbfcb3262a64f1c9d7
Philip Devine <Devine33@live.co.uk>
2020-11-26 20:41:11 +0000

    Init project

---
+++ Greed/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Greed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
